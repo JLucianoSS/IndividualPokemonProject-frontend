9aba35b3203f319ec8071205bf41bc012052f812
Jorge Luciano <jorgelss912345@gmail.com>
2023-10-30 13:59:30 -0500

    Arreglado el fondo de welcome

--- src/pokemons/pages/Welcome/WelcomePage.jsx
+++ src/pokemons/pages/Welcome/WelcomePage.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import style from "./Welcome.module.css";
-
+import img from "../../../assets/pokedex.png";
 
 export const WelcomePage = () => {
   return (
-    <div className={`${style.container_welcome} ${style.cursor_aura}`}>
-
-      <Link to="/home">
-      <button className={style.animated_button} type="button">Entrar</button>
-       
-      </Link>
-    </div>
+    <>
+      <div className={`${style.container_welcome} ${style.cursor_aura}`}>
+      <img className={style.title_pokedex} src={img} alt="pokedex"></img>
+        <Link to="/home">
+          <button className={style.animated_button} type="button">
+            Entrar
+          </button>
+        </Link>
+      </div>
+    </>
   );
 };
